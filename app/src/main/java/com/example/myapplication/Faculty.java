0647f8259e2a9a366bf62735e1c891a696b59558
Afrah Minhaj <afrahminhaj65219@gmail.com>
2020-06-23 22:42:12 +0530

    some erroe in third activity

--- app/src/main/java/com/example/myapplication/Faculty.java
+++ app/src/main/java/com/example/myapplication/Faculty.java
@@ -3,18 +3,25 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class Faculty extends AppCompatActivity {
     Button Facultyprevbutton;
+    Button Add2FAB;
+    LinearLayout FacultyLinearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faculty);
         Facultyprevbutton=(Button)findViewById(R.id.Facultyprevbutton);
+        Add2FAB=(Button)findViewById(R.id.Add2FAB);
+        FacultyLinearLayout=(LinearLayout)findViewById(R.id.FacultyLinearLayout) ;
 
         Facultyprevbutton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,6 +30,27 @@ public class Faculty extends AppCompatActivity {
                 startActivity(Facultyprev);
             }
         });
+
+        Add2FAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                add2("Faculty name","A/NA");
+            }
+        });
+
+    }
+
+    public void add2(String faculty_name, String av) {
+        LinearLayout eachfaculty=new LinearLayout(this);
+        eachfaculty.setBackgroundColor(Color.GRAY);
+        TextView Fname=new TextView(this);
+        TextView Av=new TextView(this);
+        Fname.setText(faculty_name);
+        Fname.setTextSize(20);
+        Av.setText(av);
+        eachfaculty.addView(Fname);
+        eachfaculty.addView(Av);
+        FacultyLinearLayout.addView(eachfaculty);
 
 
 
