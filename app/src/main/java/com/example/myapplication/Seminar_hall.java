1c1e689330e85f412f98f11ea843a7522b558f91
SLS Harshitha <59821143+harshitha008@users.noreply.github.com>
2020-06-24 12:57:54 +0530

    Update Seminar_hall.java

--- app/src/main/java/com/example/myapplication/Seminar_hall.java
+++ app/src/main/java/com/example/myapplication/Seminar_hall.java
@@ -4,17 +4,87 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Seminar_hall extends AppCompatActivity {
     Button SHnxtbtn;
     Button SHprevbtn;
+    private Spinner spinner;
+    private EditText seminar;
+    private EditText purpose;
+    private Button buttonConfirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seminar_hall);
+        
+        seminar = (EditText) findViewById(R.id.Semname);
+        purpose = (EditText) findViewById(R.id.purpose);
+        buttonConfirm = (Button) findViewById(R.id.submit);
+
+        seminar.addTextChangedListener(loginTextWatcher);
+        purpose.addTextChangedListener(loginTextWatcher);
+
+        spinner =findViewById(R.id.spinner);
+
+        List<String> clubs = new ArrayList<>();
+        clubs.add(0,"CHOOSE THE CLUB");
+        clubs.add("COSC");
+        clubs.add("IEEE");
+        clubs.add("Chaitanya Smruthi");
+        clubs.add("Sports");
+        clubs.add("Street Cause");
+        clubs.add("Litratri");
+
+        //style and populate the spinner
+        ArrayAdapter<String> dataAdapter;
+        dataAdapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item, clubs);
+
+        //dropdown layout style
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        //attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                if (parent.getItemAtPosition(position).equals("CHOOSE THE CLUB"))
+                {
+                    //do nothing
+                }
+                else
+                {
+                    //on selecting a spinner item
+                    String item = parent.getItemAtPosition(position).toString();
+                    //show selected spinner item
+                    Toast.makeText(parent.getContext(),"Selected: "+item, Toast.LENGTH_SHORT).show();
+
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //todo Auto-generated method stub
+
+            }
+        });
+
+
         SHnxtbtn=(Button)findViewById(R.id.SHnextButton);
         SHprevbtn=(Button)findViewById(R.id.SHprevbutton);
 
@@ -33,4 +103,25 @@ public class Seminar_hall extends AppCompatActivity {
             }
         });
     }
+    
+     private TextWatcher loginTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String hallinput = seminar.getText().toString();
+            String purposeinput = purpose.getText().toString();
+
+            buttonConfirm.setEnabled(!hallinput.isEmpty() && !purposeinput.isEmpty());
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
 }
