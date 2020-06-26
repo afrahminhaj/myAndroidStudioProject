7c365a501e88133e5e91bd2daf9ee1fad2ef102c
Afrah Minhaj <afrahminhaj65219@gmail.com>
2020-06-26 15:48:19 +0530

    ...

--- app/src/main/java/com/example/myapplication/loginpage.java
+++ app/src/main/java/com/example/myapplication/loginpage.java
@@ -6,15 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class loginpage extends AppCompatActivity {
     Button loginbtn;
+    EditText usernameET;
+    EditText pswrdET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loginpage);
         loginbtn=(Button)findViewById(R.id.loginbtn);
+        usernameET=(EditText)findViewById(R.id.usernameET);
+        pswrdET=(EditText)findViewById(R.id.passwordET);
 
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
